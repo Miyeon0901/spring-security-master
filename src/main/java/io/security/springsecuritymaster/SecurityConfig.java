1d0987bd917be7eff46c958212a248f95d799a7c
miyeon <kmy930901@etoos.com>
2025-01-06 17:01:13 +0900

    8강 사용자 정의 보안 설정하기

---
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -0,0 +1,41 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) // http 통신에 대한 인가 정책을 설정하겠다. (모든 요청)
+                .formLogin(Customizer.withDefaults()); // 인증을 받지 못했을 경우에 formLogin 방식(default)으로 인증을 받는다.
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() { // 설정 파일(yml)보다 우선시 됨.
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .roles("USER").build();
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}1111")
+                .roles("USER").build();
+        UserDetails user3 = User.withUsername("user3")
+                .password("{noop}1111")
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user, user2, user3);
+
+    }
+}
