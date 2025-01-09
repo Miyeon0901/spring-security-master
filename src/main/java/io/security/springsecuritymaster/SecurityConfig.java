eb5ed83759d7a08f04aaef1c782435beee07245e
miyeon <kmy930901@etoos.com>
2025-01-09 15:57:26 +0900

    basic

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -8,15 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
 
@@ -27,26 +23,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) // http 통신에 대한 인가 정책을 설정하겠다. (모든 요청)
-//                .formLogin(Customizer.withDefaults()); // 인증을 받지 못했을 경우에 formLogin 방식(default)으로 인증을 받는다.
-                .formLogin(form -> form
-//                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/", false) // 내부적으로 handler에서 처리.
-                        .failureUrl("/failed")
-                        .usernameParameter("userId")
-                        .passwordParameter("passwd")
-                        // successHandler, failureHandler를 정의한다면 defaultSuccessUrl, failureUrl보다 우선시 된다.
-                        .successHandler(((request, response, authentication) -> {
-                            System.out.println("authentication : " + authentication);
-                            response.sendRedirect("/home");
-                        }))
-                        .failureHandler(((request, response, exception) -> {
-                            System.out.println("exception : " + exception.getMessage());
-                            response.sendRedirect("/login");
-                        }))
-                        .permitAll()
+                .formLogin(Customizer.withDefaults()) // 인증을 받지 못했을 경우에 formLogin 방식(default)으로 인증을 받는다.
+                .rememberMe(rememberMe -> rememberMe
+                        .alwaysRemember(true)
+                        .tokenValiditySeconds(3600)
+                        .userDetailsService(userDetailsService())
+                        .rememberMeParameter("remember")
+                        .rememberMeCookieName("remember")
+                        .key("security")
                 );
-
         return http.build();
     }
 
@@ -55,14 +40,6 @@ public class SecurityConfig {
         UserDetails user = User.withUsername("user")
                 .password("{noop}1111")
                 .roles("USER").build();
-//        UserDetails user2 = User.withUsername("user2")
-//                .password("{noop}1111")
-//                .roles("USER").build();
-//        UserDetails user3 = User.withUsername("user3")
-//                .password("{noop}1111")
-//                .roles("USER").build();
-
-//        return new InMemoryUserDetailsManager(user, user2, user3);
         return new InMemoryUserDetailsManager(user);
 
     }
