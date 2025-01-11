573bba9281223ae63fdac547e132eac435c6ee76
Miyeon0901 <kmy930901@naver.com>
2025-01-11 17:11:21 +0900

    20강 인증 컨텍스트 - SecurityContext / SecurityContextHolder-2

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,13 +1,27 @@
 package io.security.springsecuritymaster;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class IndexController {
+
+    @Autowired
+    SecurityContextService securityContextService;
 
     @GetMapping("/")
     public String index() {
+        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        System.out.println("authentication = " + authentication);
+
+        securityContextService.securityContext();
+
         return "index";
     }
 
