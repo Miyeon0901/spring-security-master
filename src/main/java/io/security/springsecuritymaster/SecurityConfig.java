6a47680a2776e33715b3e9ca932523f44138a7ea
miyeon <kmy930901@etoos.com>
2025-01-15 10:00:47 +0900

    34강 세션 생성 정책 - sessionManagement().sessionCreationPolicy()

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +25,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) // http 통신에 대한 인가 정책을 설정하겠다. (모든 요청)
                 .formLogin(Customizer.withDefaults()) // 인증을 받지 못했을 경우에 formLogin 방식(default)으로 인증을 받는다.
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // IF_REQUIRED, NEVER, ALWAYS, STATELESS
                 ;
         return http.build();
     }
